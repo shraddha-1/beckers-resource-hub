090b1de60d3a088b8f2b88e088b2c5e13400dcb7
shraddha-1 <shraddhadshine192@gmail.com>
2026-05-13 04:35:21 -0600

    Fix cors trailing slash

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://beckers-resource-hub.vercel.app/',
+    'https://beckers-resource-hub.vercel.app',
   ]
 }))
 
